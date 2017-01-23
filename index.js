0494975e5ad8bba06e122544f5c3053eb0377edd
Jamin Chen <jamin.chen587@gmail.com>
2017-01-23 10:02:37 -0800

    Added function for welcome screen

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 app.listen((process.env.PORT || 3000));
+
+var APPID = '';	// Your Yahoo Application ID
+var DEG = 'c';	// c for celsius, f for fahrenheit
 
 app.get('/', function (request, response) {
     // boot boot
@@ -28,6 +32,11 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         let event = events[i];
         let sender = event.sender.id;
+        if (event.message) {
+            if (event.message.text) {
+
+            }
+        }
     }
     res.sendStatus(200);
 });
@@ -54,6 +63,29 @@ function sendMessage(recipientId, message) {
 function sendTextMessage(recipientId, text) {
     sendMessage(recipientId, {text:text});
 };
+
+function createGreeting(data) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            "setting_type":"call_to_actions",
+              "thread_state":"new_thread",
+              "call_to_actions":[
+                {
+                  "payload":"USER_DEFINED_PAYLOAD"
+                }
+              ]
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error creating greeting: ', error);
+        } else {
+            console.error('Error: ', response.body.error);
+        }
+    });
+}
 
 function promptLocation(recipientId) {
     sendMessage(recipientId, {
