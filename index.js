2f3f413b57c7e64a43901f1b8738de4ee404219c
Jamin Chen <jamin.chen587@gmail.com>
2017-01-23 08:56:33 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    request = require('request'),
+    path = require('path');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+app.get('/', function (request, response) {
+    // boot boot
+    response.send('This is weather bot.');
+});
+
+app.get('/webhook', function (request, response) {
+    // verification function
+    if (request.query['hub.verify_token'] === 'testbot_verify_token') {
+        response.send(request.query['hub.challenge']);
+    } else {
+        response.send('Invalid verify token');
+    }
+});
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        let event = events[i];
+        let sender = event.sender.id;
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+function sendTextMessage(recipientId, text) {
+    sendMessage(recipientId, {text:text});
+};
+
+function promptLocation(recipientId) {
+    sendMessage(recipientId, {
+        "text":"Where would you like to set the new location?",
+        "quick_replies":[
+          {
+            "content_type":"location",
+          }
+        ]
+    });
+};
