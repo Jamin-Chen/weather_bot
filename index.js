de558d9167d62df5345d852ef0e0823c453c236b
Jamin Chen <jamin.chen587@gmail.com>
2017-01-23 23:04:35 -0800

    testing

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ app.post('/webhook', function (req, res) {
                         console.log(lat);
                         console.log(lng);
                         weatherData = getWeather(lat, lng);
+                        console.log(weatherData.hourly.summary)
                         break;
                     }
             } else {
