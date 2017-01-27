a305bb8c29a9bfe5662b23d9c916c0d13cc21789
Jamin Chen <jamin.chen587@gmail.com>
2017-01-26 18:02:28 -0800

    changed darksky api request format

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/webhook', function (req, res) {
 });
 
 function getWeather(sender, lat, lng) {
-    url = 'https://api.darksky.net/forecast/' + apiKey + '/' + lat + ',' + lng + ',' + Date.now();
+    url = 'https://api.darksky.net/forecast/' + apiKey + '/' + lat + ',' + lng + ',' + Date.now()/1000;
     console.log(url);
     request({
         url: url,
