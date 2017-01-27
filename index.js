e53c721995c6d9372b792ffdc6dc1021ecd0c33e
Jamin Chen <jamin.chen587@gmail.com>
2017-01-27 09:50:55 -0800

    testing

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.post('/webhook', function (req, res) {
                     case "SET_LOCATION":
                         userData[sender].lat = event.message.attachments[0].payload.coordinates.lat;
                         userData[sender].lng = event.message.attachments[0].payload.coordinates.long;
+                        console.log(getOffset(sender, userData[sender].lat, userData[sender].lng));
                         timeZones[getOffset(sender, userData[sender].lat, userData[sender].lng) + 8].push(sender);
                         sendTextMessage(sender, "Got it! ^_^");
                         userData[sender].state = "DONE";
